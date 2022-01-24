214309333dc7aefc19dbd65435b7a28a470bdba4
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2022-01-23 21:04:18 -0500

    setup basic app

---
+++ artemis.py
@@ -0,0 +1,36 @@
+# This will be an AI app that takes a user's voice or text input and returns a response
+
+print("Welcome to Artemis, your personal AI assistant.")
+print("Please wait while I load my knowledge base.")
+
+# Imports
+from distutils import command
+import os
+import sys
+import time
+import random
+import speech_recognition as sr
+import pyttsx3
+import wikipedia
+import webbrowser
+import smtplib
+import requests
+import json
+import urllib.request
+import urllib.parse
+import urllib.error
+import urllib.request
+
+print("Begin by typing a command or saying something.")
+command = input("Command: ")
+
+# Begin the main loop
+while command != "quit":
+    if command == "":
+        print("You cannot say nothing.")
+        command = input("Command: ")
+
+    if command == "Hello":
+        print("Hello, how are you today?")
+        command = input("Command: ")
+
