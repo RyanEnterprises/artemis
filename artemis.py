c3497011305677ebc6d9c838680a604ab7a1bb52
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2022-01-25 15:24:21 -0500

    added many new commands

--- artemis.py
+++ artemis.py
@@ -3,10 +3,55 @@
 print("Welcome to Artemis, your personal AI assistant.")
 print("Please wait while I load my knowledge base.")
 
-#randomness
-numbers = ["1", "2", "3", "4", "5","6","7","8","9","10"]
-letters = ["a", "b", "c", "d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-coin = ["Heads", "Tails"]
+#variables
+
+versionNumber = "1.0"
+
+numbers = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10"
+]
+
+letters = [
+    "a",
+    "b",
+    "c", 
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z"
+]
+coin = [
+    "Heads",
+    "Tails"
+]
 
 
 # Imports
@@ -14,6 +59,7 @@ import os
 import sys
 import time
 import random
+from bs4 import BeautifulSoup
 import speech_recognition as sr
 import pyttsx3
 import wikipedia
@@ -45,10 +91,14 @@ while command != "quit":
 
     if command == "help":
         print("Here is a list of commands:")
+        print("'artemis' - Gives you the current version number.")
         print("'flip' - flips a coin")
         print("'Hello' - Say hello to me.")
         print("'help' - Shows a list of avalible commands")
+        print("'know' - Gives a summary from wikipedia about a topic")
         print("'open' - Opens a website.")
+        print("'play' - Plays a song.")
+        print("'prank' - It's a suprise.")
         print("'quit' - Quits the program.")
         print("'random' - Gives a random number.")
         command = input("Command: ")
@@ -60,6 +110,41 @@ while command != "quit":
     if command == "Flip a Coin" or command == "flip a coin" or command == "flip a coin for me" or command == "flip coin" or command == "flip":
         print(random.choice(coin))
         command = input("Command: ")
+
+    if command == "open":
+        print("What website do you want to open?")
+        website = input("Website: ")
+        webbrowser.open(website)
+        command = input("Command: ")
+
+    if command == 'artemis' or command == 'version' or command == 'version number' or command == 'version number for me' or command == 'about':
+        print("Artemis" + " " + versionNumber)
+        print("Artemis is a personal AI assistant.")
+        print("It is a work in progress and is still in development.")
+        print("If you have any suggestions or feedback, please let me know.")
+        print("Thank you for using Artemis.")
+        command = input("Command: ")
+
+    if command == "know":
+        print("What do you want to know?")
+        know = input("HERE: ")
+        print(wikipedia.summary(know))
+        features="html.parser"
+        page = requests.get(wikipedia.page(know).url)
+        soup = BeautifulSoup(page.content, features)
+        print(soup.get_text())
+        command = input("Command: ")
+
+    if command == "play":
+        print("What song do you want to play? TIP: It has to be on your computer.")
+        song = input("Song: ")
+        os.system("mpg321 " + song)
+        command = input("Command: ")
+
+    if command == "prank":
+        webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+        command = input("Command: ")
+    
     
     else:
         print("I'm sorry, I don't understand that command yet. However, I am always growing")
