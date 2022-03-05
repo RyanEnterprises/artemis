f22a8cacbcef25bb5d7ced5d3e9be9fab418c405
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2022-03-04 23:54:04 -0500

    added logic for artemis rename
    Signed-off-by: hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>

--- src/artemis.py
+++ src/artemis.py
@@ -32,8 +32,11 @@ else:
 
 
 print("The next step is to name me! The default name is artemis, but you can call me whatever you want.")
-artemisName = input("What do you want to call me? Leave blank if you want artemis. ")
-
+artemisUGName = input("What do you want to call me? Leave blank if you want artemis. ")
+if artemisUGName == "" or aretemisUGName == " ":
+    artemisUGName = "artemis"
+print("Are you sure you want to call me " + artemisUGName + "?")
+answerToArtemisRename = input("y/n ")
 
 
 print("Type 'voice' to use your voice. TIP: Only use this if you have the PyAudio module installed.")
