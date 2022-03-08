eab4efa451441ea828e3d15d55a2f1180fefc6a3
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2022-03-08 13:56:27 -0500

    restart project

---
+++ src/js/artemis.js
@@ -0,0 +1,28 @@
+var input = document.getElementById('input box');
+var submit = document.getElementById('submit button');
+// when submit is clicked, take the text and run a function
+submit.addEventListener('click', function () {
+    // get the text from the input box
+    var text = input.value;
+    // clear the input box
+    input.value = '';
+    // run the function
+    console.log(text);
+    if (text != ' ') {
+        if (text.toLowerCase() == 'hello') {
+            console.log(Math.floor(Math.random() * hellos.length));
+        }
+    }
+    ;
+});
+// Arrays
+var hellos = [
+    'Hello',
+    'Bonjour',
+    'Hola',
+    'Hallo',
+    'Ciao',
+    'Salut',
+    'Guten Tag',
+    'Namaste',
+];
---
+++ src/ts/artemis.ts
@@ -0,0 +1,32 @@
+var input = document.getElementById('input box');
+var submit = document.getElementById('submit button');
+
+// when submit is clicked, take the text and run a function
+submit.addEventListener('click', function () {
+    // get the text from the input box
+    var text = input.value;
+    // clear the input box
+    input.value = '';
+    // run the function
+    console.log(text);
+    if (text != ' ') {
+        if (text.toLowerCase() == 'hello') {
+            console.log(Math.floor(Math.random() * hellos.length));
+        }
+    };
+});
+
+
+
+// Arrays
+
+const hellos = [
+    'Hello',
+    'Bonjour',
+    'Hola',
+    'Hallo',
+    'Ciao',
+    'Salut',
+    'Guten Tag',
+    'Namaste',
+]
