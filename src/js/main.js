3b2e7333f6283add511bad80adafe096bc1e3420
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2022-03-11 09:36:10 -0500

    UGH restart project

--- src/js/main.js
+++
@@ -1,8 +0,0 @@
-var input = document.getElementById('input box');
-var submit = document.getElementById('submit button');
-// have the user type something in the box, and then when submit is clicked, trigger a function
-input.addEventListener('keyup', function (event) {
-    if (event.key === 'Enter') {
-        submit.click();
-    }
-});
