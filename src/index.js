7e6d2b75a57b8a14df4dac693be83093e18864b9
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2022-03-12 16:44:10 -0500

    feat: add about window

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const { app, BrowserWindow, Menu } = require('electron');
 const path = require('path');
 const os = require('os');
 const { shell } = require('electron');
+const openAboutWindow = require('about-window').default;
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
@@ -43,6 +44,171 @@ app.on('activate', () => {
     createWindow();
   }
 });
+
+
+
+const menu = Menu.buildFromTemplate([
+  {
+    label: 'File',
+    submenu: [
+      {
+        label: 'New',
+        accelerator: 'CmdOrCtrl+N',
+        click: () => {
+          console.log('New File');
+        }
+      },
+      {
+        label: 'Open',
+        accelerator: 'CmdOrCtrl+O',
+        click: () => {
+          console.log('Open File');
+        }
+      },
+      {
+        label: 'Save',
+        accelerator: 'CmdOrCtrl+S',
+        click: () => {
+          console.log('Save File');
+        }
+      },
+      {
+        label: 'Save As',
+        click: () => {
+          console.log('Save As File');
+        }
+      },
+      {
+        label: 'Close',
+        click: () => {
+          console.log('Close File');
+        }
+      },
+      {
+        label: 'Quit',
+        accelerator: 'CmdOrCtrl+Q',
+        click: () => {
+          app.quit();
+        }
+      }
+    ]
+  },
+  {
+    label: 'Edit',
+    submenu: [
+      {
+        label: 'Cut',
+        accelerator: 'CmdOrCtrl+X',
+        click: () => {
+          console.log('Cut');
+        }
+      },
+      {
+        label: 'Copy',
+        accelerator: 'CmdOrCtrl+C',
+        click: () => {
+          console.log('Copy');
+        }
+      },
+      {
+        label: 'Paste',
+        accelerator: 'CmdOrCtrl+V',
+        click: () => {
+          console.log('Paste');
+        }
+      },
+      {
+        label: 'Select All',
+        accelerator: 'CmdOrCtrl+A',
+        click: () => {
+          console.log('Select All');
+        }
+      }
+    ]
+  },
+  {
+    label: 'View',
+    submenu: [
+      {
+        label: 'Reload',
+        accelerator: 'CmdOrCtrl+R',
+        click: () => {
+          BrowserWindow.getFocusedWindow().reload();
+        }
+      },
+      {
+        label: 'Toggle Developer Tools',
+        accelerator: 'Alt+CmdOrCtrl+I',
+        click: () => {
+          BrowserWindow.getFocusedWindow().webContents.toggleDevTools();
+        }
+      },
+      {
+        label: 'Toggle Full Screen',
+        accelerator: 'CmdOrCtrl+Shift+F',
+        click: () => {
+          BrowserWindow.getFocusedWindow().setFullScreen(!BrowserWindow.getFocusedWindow().isFullScreen());
+        }
+      }
+    ]
+  },
+  {
+    label: 'Window',
+    submenu: [
+      {
+        label: 'Minimize',
+        accelerator: 'CmdOrCtrl+M',
+        click: () => {
+          BrowserWindow.getFocusedWindow().minimize();
+        }
+      },
+      {
+        label: 'Close',
+        accelerator: 'CmdOrCtrl+W',
+        click: () => {
+          BrowserWindow.getFocusedWindow().close();
+        }
+      }
+    ]
+  },
+  {
+    label: 'Help',
+    submenu: [
+      {
+        label: 'Github',
+        click: () => {
+          shell.openExternal('https://github.com/RyanEnterprises/artemis');
+        }
+      },
+      {
+        label: 'About Artemis',
+        click: () => {
+          openAboutWindow({
+            icon_path: path.join(__dirname, 'images/FullColorIcon.png'),
+            copyright: 'Copyright © 2022 Ryan Enterprises',
+            package_json_dir: __dirname,
+            use_version_info: [
+              ['Artemis', '5.0.0'],
+              ['node.js', '17.6.0'],
+              ['electron', '7.1.6'],
+              ['v8', '7.6.0'],
+              ['chromium', '75.0.3770.90'],
+            ],})
+        }
+      }
+    ]
+  }
+]);
+
+
+
+
+
+
+
+
+
+
 
 
 
